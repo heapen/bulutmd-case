8963f489eabe6bc05c317c2481616b3e29ef425e
Batuhan Celik <alparslanbatuhancelik@gmail.com>
2023-08-24 23:38:31 +0300

    Feat [Movie] : Movie List Add

--- src/components/ShowsCard/ShowsCard.jsx
+++ src/components/ShowsCard/ShowsCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const ShowsCard = ({ title, description, image, releaseDate }) => {
+const ShowsCard = ({ title, description, poster, releaseDate }) => {
     const [hovered, setHovered] = useState(false);
 
     return (
@@ -12,17 +12,17 @@ const ShowsCard = ({ title, description, image, releaseDate }) => {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <img src={'https://upload.wikimedia.org/wikipedia/en/1/1f/Mad_Dogs_Blu-ray.jpg'} className="w-full" alt={`${'title'} Poster`} />
+      <img src={poster} className="w-full" alt={`${title} Poster`} />
       <div className="px-6 py-4">
         <div className={`font-bold text-white text-xl mb-2 ${hovered ? 'mr-4' : ''}`}>
-          {'title'}
+          {title}
         </div>
-        {hovered && <p className="text-white text-base">{'description'}</p>}
+        {hovered && <p className="text-white text-base">{description}</p>}
       </div>
       {hovered && (
         <div className="px-6 py-4">
           <span className="inline-block bg-red-700 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">
-            {'releaseDate'}
+            {releaseDate}
           </span>
         </div>
       )}
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import ShowsCard from '../components/ShowsCard/ShowsCard'
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import ShowsCard from '../components/ShowsCard/ShowsCard';
+import { useSelector } from "react-redux";
+import { listData } from '../redux/dataSlice/dataSlice';
 
 const Movies = () => {
+  const allData = useSelector(listData);
+
+  // Filmleri filtreleyip sadece programType: "movie" olanları alıyoruz
+  const movies = allData.filter(item => item.programType === "movie");
+
+  // İlk 18 filmi alıyoruz
+  const first18Movies = movies.slice(0, 18);
+
   return (
     <>
-    <Navbar/>
-    <div className='flex flex-wrap justify-center items-center h-screen space-x-4'>
-        <ShowsCard/>
-        <ShowsCard/>
-        <ShowsCard/>
-        <ShowsCard/>
-        <ShowsCard/>
-        <ShowsCard/>
-    </div>
+      <Navbar />
+      <div className="flex flex-wrap justify-center items-center h-screen">
+        {first18Movies.map((item) => (
+          <ShowsCard
+            key={item.id}
+            poster={item.images["Poster Art"].url}
+            description={item.description}
+            title={item.title}
+            releaseDate={item.releaseYear}
+          />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Movies
+export default Movies;
